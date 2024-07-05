a771a921a63bb14fd1d5e64163aec858f4ec3759
tian <istiancz@gmail.com>
2024-07-05 10:01:33 +0800

    fix: error msg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,9 +11,6 @@ export async function checkEnv() {
     return raw ? parseInt(raw[2], 10) : 0;
   }
 
-  // @ts-expect-error
-  const state = await ai?.canCreateTextSession();
-  if (state === "readily") return;
   const version = getChromeVersion();
   if (version < 127) {
     throw new Error(
@@ -27,6 +24,8 @@ export async function checkEnv() {
     );
   }
 
+  // @ts-expect-error
+  const state = await ai?.canCreateTextSession();
   if (state !== "readily") {
     throw new Error(
       "Built-in AI is not ready, check your configuration in chrome://flags/#optimization-guide-on-device-model",
