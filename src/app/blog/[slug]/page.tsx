f8f0c70ad931e284eb4ab400a0638af0690385d1
tian <istiancz@gmail.com>
2024-07-09 11:03:45 +0800

    feat: update blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,29 +1,30 @@
 import React from "react";
-import { blogList } from "@/lib/blogs";
 import Markdown from "react-markdown";
-import { convertParamToTitle, convertTitleToPath } from "@/lib/utils";
+import { promises as fs } from "fs";
 
 export const dynamicParams = false;
 export async function generateStaticParams() {
-  const list = blogList.map((post) => ({
-    slug: convertTitleToPath(post.title),
-  }));
-
-  return list;
+  const list = await fs.readdir("./src/blogs");
+  const result = await Promise.all(
+    list.map(async (l) => {
+      const path = l.replace(".md", "");
+      return {
+        slug: path,
+      };
+    }),
+  );
+  return result;
 }
+const getBlogByPath = async (path: string) => {
+  const file = await fs.readFile("./src/blogs/" + path + ".md", "utf-8");
+  return file;
+};
 
-export default function page({ params }: { params: { slug: string } }) {
-  const page = getBlog(params.slug);
+export default async function page({ params }: { params: { slug: string } }) {
+  const content = await getBlogByPath(params.slug);
   return (
     <article className="w-full max-w-3xl mx-auto prose px-4 py-6">
-      <h1 className="text-5xl font-bold">{page?.title}</h1>
-      <Markdown>{page?.content}</Markdown>
+      <Markdown>{content}</Markdown>
     </article>
   );
-}
-
-function getBlog(id: string) {
-  const path = convertParamToTitle(id);
-  const page = blogList.find((l) => l.title === decodeURIComponent(path));
-  return page;
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,37 @@
 import Link from "next/link";
 import React from "react";
-import { blogList } from "@/lib/blogs";
-import { convertTitleToPath } from "@/lib/utils";
+import { promises as fs } from "fs";
+import dayjs from "dayjs";
 
-export default function page() {
+export default async function page() {
+  const list = await getBlogList();
   return (
     <div className="w-full max-w-3xl mx-auto py-6 px-4">
-      {blogList.map((l, index) => (
+      {list.map((l, index) => (
         <div key={index} className="mb-4">
-          <Link className="block" href={`/blog/${convertTitleToPath(l.title)}`}>
+          <Link className="block" href={`/blog/${l.path}`}>
             <h1 className="underline text-2xl">{l.title}</h1>
           </Link>
-          <p>{l.date}</p>
+          <p>{dayjs(l.date).format("YYYY-MM-DD HH:mm")}</p>
         </div>
       ))}
     </div>
   );
 }
+
+const getBlogList = async () => {
+  const list = await fs.readdir("./src/blogs");
+  const result = await Promise.all(
+    list.map(async (l) => {
+      const d = await fs.stat("./src/blogs/" + l);
+      const path = l.replace(".md", "");
+      const title = path.split("-").join(" ");
+      return {
+        title,
+        path,
+        date: d.mtime,
+      };
+    }),
+  );
+  return result;
+};
