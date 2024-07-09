c0b97110af962eac349a8890604ffe9f1e6bdc2d
tian <istiancz@gmail.com>
2024-07-09 14:28:07 +0800

    feat: render video in md

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Markdown from "react-markdown";
 import { promises as fs } from "fs";
+import rehypeRaw from "rehype-raw";
 
 export const dynamicParams = false;
 export async function generateStaticParams() {
@@ -24,7 +25,7 @@ export default async function page({ params }: { params: { slug: string } }) {
   const content = await getBlogByPath(params.slug);
   return (
     <article className="w-full max-w-3xl mx-auto prose px-4 py-6">
-      <Markdown>{content}</Markdown>
+      <Markdown rehypePlugins={[rehypeRaw]}>{content}</Markdown>
     </article>
   );
 }
