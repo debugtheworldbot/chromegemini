ab77c2151f9f092bc82cbf66145a951a37c70b29
tian <istiancz@gmail.com>
2024-07-10 15:44:15 +0800

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,14 @@
 import { MetadataRoute } from "next";
+import { promises as fs } from "fs";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogPaths = await getBlogPaths();
+  const blogs = blogPaths.map((path) => ({
+    url: `https://chromeai.org/blog/${path}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.6,
+  })) as MetadataRoute.Sitemap;
   return [
     {
       url: "https://chromeai.org",
@@ -14,5 +22,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.6,
     },
+    {
+      url: "https://chromeai.org/blog",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    ...blogs,
   ];
 }
+const getBlogPaths = async () => {
+  const list = await fs.readdir("./src/blogs");
+  const result = list.map((l) => {
+    const path = l.replace(".md", "");
+    return path;
+  });
+
+  return result;
+};
