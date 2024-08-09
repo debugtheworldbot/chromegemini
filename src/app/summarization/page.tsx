d028292a71e95037981c2331ccc7b1e2b9bebb14
tian <istiancz@gmail.com>
2024-08-09 11:28:54 +0800

    feat: add help

--- src/app/summarization/page.tsx
+++ src/app/summarization/page.tsx
@@ -2,38 +2,64 @@
 import { CodeSnippet } from "@/components/codeSnippet";
 import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
+import { HoverCard, HoverCardContent } from "@/components/ui/hover-card";
 import { Textarea } from "@/components/ui/textarea";
 import { useSummarize } from "@/hooks/use-summarize";
-import { Loader } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { HoverCardTrigger } from "@radix-ui/react-hover-card";
+import { BadgeHelp, Loader } from "lucide-react";
 import React, { useState } from "react";
 
 export default function Page() {
   const { canSummarize, summarize, error, checking } = useSummarize();
   const [result, setResult] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const [time, setTime] = useState(0);
   return (
     <div className="min-h-screen w-screen">
       <Header />
       <h1 className="text-center text-4xl font-medium mt-8">
         Summarization by Gemini Nano
+        <HoverCard>
+          <HoverCardTrigger>
+            <BadgeHelp className="inline-block align-top ml-2 cursor-pointer hover:underline" />
+          </HoverCardTrigger>
+          <HoverCardContent className="pl-8 md:w-96">
+            <h2 className="text-xl">Caveats</h2>
+            <ul className="list-disc text-base font-normal text-left">
+              <li>Only English input and output are supported.</li>
+              <li>
+                No support of any options (e.g. length guidance, style, etc) for
+                the time being.
+              </li>
+              <li>
+                The context window is currently limited to 1024 tokens but we
+                use about 26 of those under the hood.
+                <p> 1 token is roughly equal to 4 English characters.</p>
+              </li>
+            </ul>
+          </HoverCardContent>
+        </HoverCard>
       </h1>
       {checking ? (
         <p className="text-2xl font-medium flex justify-center items-center mt-12">
           <Loader className="animate-spin mr-2" /> Checking your browser...
         </p>
       ) : canSummarize ? (
-        <main className="flex h-screen overflow-scroll flex-1 flex-col">
+        <main className="overflow-scroll">
           <form
             onSubmit={async (e) => {
               e.preventDefault();
               const formData = new FormData(e.currentTarget);
               const input = formData.get("input") as string;
               setLoading(true);
+              const startTime = performance.now();
               const res = await summarize(input);
+              setTime(performance.now() - startTime);
               setResult(res);
               setLoading(false);
             }}
-            className="px-12 mt-8"
+            className="lg:max-w-5xl mt-8 lg:mx-auto mx-8"
           >
             <Textarea name="input" rows={10} required />
             <Button
@@ -53,7 +79,23 @@ export default function Page() {
           </form>
           {result && (
             <div className="mx-12">
-              <h2 className="text-2xl font-medium mt-8">Summary</h2>
+              <h2 className="text-2xl font-medium mt-8">
+                Summary
+                {time > 0 && (
+                  <span
+                    className={cn(
+                      time > 5000
+                        ? "text-red-500"
+                        : time > 1000
+                          ? "text-yellow-400"
+                          : "text-green-500",
+                      "ml-2",
+                    )}
+                  >
+                    {time.toFixed(0)} ms
+                  </span>
+                )}
+              </h2>
               <div className="mt-4 text-lg bg-[#f5f5f5] p-4 rounded-xl">
                 {result}
               </div>
